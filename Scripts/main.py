9ca6f933c6617c9114fc10b9a9a361a9a644922d
Maria Brabham <mattbrabham@hotmail.com>
2021-04-25 19:35:22 +0100

    basic noise, no smoothing

--- Scripts/main.py
+++ Scripts/main.py
@@ -4,11 +4,8 @@ pygame.init()
 
 #Variables
 simSize = (60,60)
-simGrid = [[0 for i in range(60)] for j in range(60)]
 simColours = {0: (26, 148, 49), 1: (8, 96, 168)}
 agentLocation = (0,0)
-
-worldSeed = 0
 
 window = pygame.display.set_mode((simSize[0] * 8, simSize[1] * 8))
 pygame.display.set_caption("Weird Survival Game")
@@ -17,18 +14,24 @@ pygame.display.set_caption("Weird Survival Game")
 def DrawWorld(m):
   for y in range(0, 60):
     for x in range(0, 60):
-      pygame.draw.rect(window, simColours[m[x][y]], ((x * 8), (simSize[1] * 8) - (y * 8) - 8, 8, 8))
+      tileType = 0
+      value = m[x][y]
+
+      if value < 0.7:
+        tileType = 0
+      else:
+        tileType = 1
+      pygame.draw.rect(window, (255 * value, 255 * value, 255 * value), ((x * 8), (simSize[1] * 8) - (y * 8) - 8, 8, 8))
 
 world = worldClass.worldMap(60)
-world.genMap()
+world.genMap(69)
 
-print(world.mapArray)
-#DrawWorld(world.mapArray)
+#print(world.mapArray)
+DrawWorld(world.mapArray)
 
 #Main loop
 running = True
 while running == True:
-  #window.fill((26, 148, 49))
   #DrawWorld(simGrid)
 
   for event in pygame.event.get():
--- Scripts/worldClass.py
+++ Scripts/worldClass.py
@@ -1,11 +1,11 @@
-import random, json
+import random
 
 class worldMap():
     def __init__(self, size):
         self.arraySize = size
         self.mapArray = [[-1 for i in range(size)] for j in range(size)]
 
-    def shitStartGen(self, x, y):
+    def fuckingShittyDepreciatedRecurssion(self, x, y):
         if y == self.arraySize:
             return
         if x == 0:
@@ -25,20 +25,15 @@ class worldMap():
         if x != self.arraySize:
             self.startGen(x, y + 1)
 
-    def genMap(self):
-        #for x in range(0,self.arraySize):
-        #    self.startGen(x, 0)
-        #print(self.mapArray)
-
+    def genMap(self, seed):
+        random.seed(seed)
         for y in range(0, self.arraySize):
             for x in range(0, self.arraySize):
-                if x == 0 and y == 0:
-                    self.mapArray[x][y] = round(random.random(),2)
-                else:
-                    rand = round(random.random(),2)
-                    if rand >= 0.1 + self.mapArray[x - 1][y]: #Above
-                        rand = 0.1 + self.mapArray[x - 1][y]
-                    elif rand <= self.mapArray[x - 1][y] - 0.1: #Below
-                        rand = self.mapArray[x - 1][y] - 0.1
-                    else:
-                        self.mapArray[x][y] = rand                    
+                self.mapArray[x][y] = round(random.random(),2)
+
+        self.smoothGen
+
+    def smoothGen(self):
+
+                
+                
