5c2eb4d4e4d9b5c665433f28fbdbdfa8a75fe8d9
Maria Brabham <mattbrabham@hotmail.com>
2021-04-26 12:22:52 +0100

    params file

--- Scripts/main.py
+++ Scripts/main.py
@@ -1,10 +1,11 @@
-import pygame, random
+import pygame, random, json
 import worldClass
 pygame.init()
 
 #Variables
-simSize = 256
-gridSize = 2
+simSize = 128
+gridSize = 8
+simSeed = 420
 
 simColours = {0: (8, 96, 168), 1: (245, 234, 146), 2: (26, 148, 49), 3: (136, 140, 141), 4: (96, 103, 107), 5: (255, 250, 250)}
 agentLocation = (0,0)
@@ -17,33 +18,18 @@ pygame.display.set_caption("Weird Survival Game")
 def DrawWorld(m):
   for y in range(0, simSize):
     for x in range(0, simSize):
-      tileType = 0
-      value = m[x][y]
-      value = 1 - value
-
-      if value < 0.016:
-        colour = simColours[0] # Water
-      elif value < 0.025:
-        colour = simColours[1] # Beach
-      elif value < 0.35:
-        colour = simColours[2] # Land
-      elif value < 0.53:
-        colour = simColours[3] # Stone1
-      elif value < 0.7:
-        colour = simColours[4] # Stone2      
-      elif value < 1:
-        colour = simColours[5] # Mountain
-
       #simColours[tileType]
       #255 * value, 255 * value, 255 * value
+
+      colour = world.typeArray[x][y]
       pygame.draw.rect(window2, (colour), ((x * gridSize), (y * gridSize) - gridSize, gridSize, gridSize))
-      #pygame.display.update()
 
+def RandomWorld():
+  world.genMap(random.randint(0, 10000))
+  DrawWorld(world.heightArray)
+
+#Setup
 world = worldClass.worldMap(simSize)
-world.genMap(1024)
-
-#print(world.mapArray)
-DrawWorld(world.mapArray)
 
 #Main loop
 running = True
@@ -53,5 +39,8 @@ while running == True:
   for event in pygame.event.get():
     if event.type == pygame.QUIT:
       running = False
+    elif event.type == pygame.KEYDOWN:
+      if event.key == pygame.K_RETURN:
+        RandomWorld()
 
   pygame.display.update()
--- Scripts/worldClass.py
+++ Scripts/worldClass.py
@@ -1,50 +1,64 @@
-import random
+import random, json
 
 class worldMap():
     def __init__(self, size):
         self.arraySize = size
-        self.mapArray = [[-1 for i in range(size)] for j in range(size)]
+        self.heightArray = [[-1 for i in range(size)] for j in range(size)]
+        self.typeArray = [[(0, 0, 0) for i in range(size)] for j in range(size)]
 
-    def fuckingShittyDepreciatedRecursion(self, x, y):
-        if y == self.arraySize:
-            return
-        if x == 0:
-            self.mapArray[x][y] = round(random.random(),2)
-            print(str(x) + " " + str(y) + " " + str(self.mapArray[x][y]))
-        else:
-            while self.mapArray[x - 1][y] == -1:
-                pass
-            rand = round(random.random(),2)
-            if rand >= 0.1 + self.mapArray[x - 1][y]: #Above
-                rand = 0.1 + self.mapArray[x - 1][y]
-            elif rand <= self.mapArray[x - 1][y] - 0.1: #Below
-                rand = self.mapArray[x - 1][y] - 0.1
-            else:
-                self.mapArray[x][y] = rand
+        self.inverted = False
+        self.upNeutralDown = 0
+        self.averaging = 0
+        self.params = []
+        self.thresholds = []
+        self.loadParameters("DefaultParameters.json")
+
+    def loadParameters(self, fname):
+        file = open("Presets\\{}".format(fname), "r")
+        self.params = json.loads(file.read())
+        file.close()
         
-        if x != self.arraySize:
-            self.startGen(x, y + 1)
+        for key in self.params:
+            if key == "Inverted":
+                if self.params[key] == 1:
+                    self.inverted = True
+            elif key == "UpNeutralDown":
+                self.upNeutralDown = self.params[key]
+            elif key == "Averaging":
+                self.averaging = self.params[key]
+            else:
+                self.thresholds.append((float(key),(self.params[key][0], self.params[key][1], self.params[key][2])))
+
+    def convertTypes(self):
+        for y in range(0, self.arraySize):
+            for x in range(0, self.arraySize):
+                for i in range(len(self.thresholds)):
+                    value = self.heightArray[x][y]
+                    if self.inverted:
+                        value = 1 - value
+                    if value <= self.thresholds[i][0]:
+                        #print(thresholds[i][0])
+                        self.typeArray[x][y] = self.thresholds[i][1]
+                        break
+            
 
     def genMap(self, seed):
         random.seed(seed)
         for y in range(0, self.arraySize):
             for x in range(0, self.arraySize):
-                self.mapArray[x][y] = round(random.random(),2)
+                self.heightArray[x][y] = round(random.random(),2)
 
-        self.upNeutralDown()
-        self.upNeutralDown()
-        self.upNeutralDown()
-        self.upNeutralDown()
+        for i in range(self.upNeutralDown):
+            self.upNeutralDownGen()
+            #print("UNDGen")
+        for i in range(self.averaging):
+            self.averageGen()
+            #print("averaging")       
 
-        self.smoothGen()
-        self.smoothGen()
-        self.smoothGen()
-        self.smoothGen()
-        self.smoothGen()
-        self.smoothGen()
+        self.convertTypes()
 
-    def upNeutralDown(self):
-        dupMap = self.mapArray
+    def upNeutralDownGen(self):
+        dupMap = self.heightArray
         for y in range(0, self.arraySize):
             for x in range(0, self.arraySize):
                 up = 0
@@ -53,26 +67,26 @@ class worldMap():
                 pointArr = []
 
                 if x != 0 and y != 0:
-                    pointArr.append(self.mapArray[x - 1][y - 1])
+                    pointArr.append(self.heightArray[x - 1][y - 1])
                 if x != 0 and y != self.arraySize - 1:
-                    pointArr.append(self.mapArray[x - 1][y + 1])
+                    pointArr.append(self.heightArray[x - 1][y + 1])
                 if x != self.arraySize - 1 and y != self.arraySize - 1:
-                    pointArr.append(self.mapArray[x + 1][y + 1])
+                    pointArr.append(self.heightArray[x + 1][y + 1])
                 if x != self.arraySize - 1 and y != 0:
-                    pointArr.append(self.mapArray[x + 1][y - 1])
+                    pointArr.append(self.heightArray[x + 1][y - 1])
                 if x != 0:
-                    pointArr.append(self.mapArray[x - 1][y])
+                    pointArr.append(self.heightArray[x - 1][y])
                 if y != 0:
-                    pointArr.append(self.mapArray[x][y - 1])
+                    pointArr.append(self.heightArray[x][y - 1])
                 if x != self.arraySize - 1:
-                    pointArr.append(self.mapArray[x + 1][y])
+                    pointArr.append(self.heightArray[x + 1][y])
                 if y != self.arraySize - 1:
-                    pointArr.append(self.mapArray[x][y + 1])
+                    pointArr.append(self.heightArray[x][y + 1])
 
                 for i in range(len(pointArr)):
-                    if pointArr[i] >= self.mapArray[x][y] + 0.1:
+                    if pointArr[i] >= self.heightArray[x][y] + 0.1:
                         up += 1
-                    elif pointArr[i] <= self.mapArray[x][y] - 0.1:
+                    elif pointArr[i] <= self.heightArray[x][y] - 0.1:
                         down += 1
                     else:
                         neutral += 1
@@ -87,41 +101,41 @@ class worldMap():
                 dupMap[x][y] += value
                 dupMap[x][y] = self.clamp(dupMap[x][y], 0, 1)
 
-        self.mapArray = dupMap
+        self.heightArray = dupMap
 
-    def smoothGen(self):
-        dupMap = self.mapArray
+    def averageGen(self):
+        dupMap = self.heightArray
         for y in range(0, self.arraySize):
             for x in range(0, self.arraySize):        
                 total = 0
                 count = 0
                 if x != 0 and y != 0:
-                    total += self.mapArray[x - 1][y - 1]
+                    total += self.heightArray[x - 1][y - 1]
                     count += 1
                 if x != 0 and y != self.arraySize - 1:
-                    total += self.mapArray[x - 1][y + 1]
+                    total += self.heightArray[x - 1][y + 1]
                     count += 1
                 if x != self.arraySize - 1 and y != self.arraySize - 1:
-                    total += self.mapArray[x + 1][y + 1]
+                    total += self.heightArray[x + 1][y + 1]
                     count += 1
                 if x != self.arraySize - 1 and y != 0:
-                    total += self.mapArray[x + 1][y - 1]
+                    total += self.heightArray[x + 1][y - 1]
                     count += 1
                 if x != 0:
-                    total += self.mapArray[x - 1][y]
+                    total += self.heightArray[x - 1][y]
                     count += 1
                 if y != 0:
-                    total += self.mapArray[x][y - 1]
+                    total += self.heightArray[x][y - 1]
                     count += 1
                 if x != self.arraySize - 1:
-                    total += self.mapArray[x + 1][y]
+                    total += self.heightArray[x + 1][y]
                     count += 1
                 if y != self.arraySize - 1:
-                    total += self.mapArray[x][y + 1]
+                    total += self.heightArray[x][y + 1]
                     count += 1
 
                 dupMap[x][y] = total / count
-        self.mapArray = dupMap
+        self.heightArray = dupMap
 
     def clamp(self, val, low, high):
         return low if val < low else high if val > high else val
