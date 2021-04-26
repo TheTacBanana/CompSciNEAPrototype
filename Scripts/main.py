bdc9f6d298a672169e154815ce5d255d75e2a63c
Maria Brabham <mattbrabham@hotmail.com>
2021-04-26 22:32:02 +0100

    Notes and screenshots

--- Scripts/main.py
+++ Scripts/main.py
@@ -5,30 +5,31 @@ import worldClass
 
 #Variables
 simSize = 256
-gridSize = 1
+gridSize = 2
 simSeed = 420
 
 #World Functions
 def DrawWorld(m):
-  if world.grayscale == False:
+  if world.grayscale == False: # Split into an if statement for optimisation, doesnt need to check if its grayscale after drawing every pixel
     for y in range(0, simSize):
       for x in range(0, simSize):
         colour = world.typeArray[x][y]
-        pygame.draw.rect(window, (colour), ((x * gridSize), (y * gridSize) - gridSize, gridSize, gridSize))
+        pygame.draw.rect(window, (colour), ((x * gridSize), (y * gridSize), gridSize, gridSize))
   else:
     for y in range(0, simSize):
       for x in range(0, simSize):
         value = world.heightArray[x][y]
-        pygame.draw.rect(window, (255 * value, 255 * value, 255 * value), ((x * gridSize), (y * gridSize) - gridSize, gridSize, gridSize))
+        pygame.draw.rect(window, (255 * value, 255 * value, 255 * value), ((x * gridSize), (y * gridSize), gridSize, gridSize))
 
 def RandomWorld():
   SetWorld(random.randint(0, 10000))
 
 def SetWorld(seed):
-  print(datetime.now())
+  #print(datetime.now().time())
   world.genMap(seed)
+  #print(datetime.now().time())
   DrawWorld(world.heightArray)
-  print(datetime.now())
+  #print(datetime.now().time())
 
 #Setup
 window = pygame.display.set_mode((simSize * gridSize, simSize * gridSize))
